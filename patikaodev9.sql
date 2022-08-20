15c9a74bf6ee4fa19c1db1b8b968b850982eb7e1
mmgldemir <55104826+mmgldemir@users.noreply.github.com>
2022-08-20 15:59:57 +0300

    Create patikaodev9.sql

---
+++ patikaodev9.sql
@@ -0,0 +1,8 @@
+--select city, country from city
+--inner join country on country.country_id=city.country_id;
+
+--select payment.payment_id, customer.first_name, customer.last_name from customer
+--inner join payment on payment.customer_id=customer.customer_id;
+
+--select rental_id, first_name, last_name from customer
+--inner join rental on rental.customer_id = customer.customer_id;
